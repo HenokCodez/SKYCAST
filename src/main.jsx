18e8971fa68b90b451d3cdbf26c188ca77de1088
Henok Binayew <henokbinayew21@gmail.com>
2025-02-26 16:17:51 +0300

    SkyCast V-1.0

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/SKYCAST/">
-    // Makes theme & unit settings available globally across the app.
     <ThemeProvider>
       <App />
     </ThemeProvider>
